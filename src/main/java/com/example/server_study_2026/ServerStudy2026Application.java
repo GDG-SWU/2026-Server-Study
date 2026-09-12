f69583e753ba9b31d5217c971bdb7c0953047534
sebin0204 <pjjanguou@gmail.com>
2026-09-12 16:33:40 +0900

    chore: 초기 세팅

---
+++ src/main/java/com/example/server_study_2026/ServerStudy2026Application.java
@@ -0,0 +1,13 @@
+package com.example.server_study_2026;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerStudy2026Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServerStudy2026Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/server_study_2026/ServerStudy2026ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.server_study_2026;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerStudy2026ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
